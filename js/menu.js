20e5d532124c3d651d10974cd43685dc50ee94cd
Tomasz Kucharz <tkucharz@frontendwebsite.com>
2022-07-08 20:06:18 +0200

    08.07.2022

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,16 @@
 const yachts = document.querySelector(`.yachts`);
+const destination = document.querySelector(`.destinations`);
+const menuLi = document.querySelector(`.menuLi`);
+const contact = document.querySelector(`.contactForUs`);
 const bigBtns = [...document.querySelectorAll(`.bigBtn`)];
 
-const handleClik = (el) => {
-  bigBtns.forEach((el) => {
-    el.classList.add("yachtOn");
-  });
+const handleClik = () => {
+	bigBtns.forEach((el) => {
+		el.classList.add('yachtOn');
+	});
 };
 
 yachts.addEventListener(`click`, handleClik);
+destination.addEventListener(`click`, handleClik);
+menuLi.addEventListener(`click`, showMenu);
+contact.addEventListener(`click`, showContact);
