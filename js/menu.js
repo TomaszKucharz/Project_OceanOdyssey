8bebb817a22ce84dc98e0ee6590cf8fa2a6cb1d2
Tomasz Kucharz <tkucharz@frontendwebsite.com>
2022-07-11 00:25:44 +0200

    10.07.2022

--- js/menu.js
+++ js/menu.js
@@ -5,30 +5,49 @@ const contact = document.querySelector(`.contactForUs`);
 const bigBtns = [...document.querySelectorAll(`.bigBtn`)];
 
 const handleClickYachts = () => {
-	bigBtns.forEach((el) => {
-		if (el.classList.contains(`yacht`)) {
-			el.classList.toggle(`yachtOn`);
-		} else if (el.classList.contains(`destination`) & el.classList.contains(`yachtOn`)) {
-			el.classList.add(`clearYachtOn`);
-		}
-	});
+  bigBtns.forEach((el) => {
+    if (
+      el.classList.contains(`destination`) & el.classList.contains(`yachtOn`)
+    ) {
+      el.classList.add(`clearYachtOn`);
+      yachtX();
+      setTimeout(() => {
+        el.classList.remove(`yachtOn`, `clearYachtOn`);
+      }, 510);
+    }
+    if (el.classList.contains(`yacht`)) {
+      el.classList.toggle(`yachtOn`);
+    }
+  });
 };
 const handleClickDestination = () => {
-	bigBtns.forEach((el) => {
-		if (el.classList.contains(`destination`)) {
-			el.classList.toggle(`yachtOn`);
-		} else if (el.classList.contains(`yacht`) & el.classList.contains(`yachtOn`)) {
-			el.classList.add(`clearYachtOn`);
-			// bigBtns.forEach((el) => {
-			// 	if (el.classList.contains(`clearYachtOn`)) {
-			// 		el.classList.remove(`clearYachtOn`);
-			// 		el.classList.remove(`yachtOn`);
-			// 	}
-			// });
-		}
-	});
+  bigBtns.forEach((el) => {
+    if (el.classList.contains(`destination`)) {
+      el.classList.toggle(`yachtOn`);
+    }
+    if (el.classList.contains(`yacht`) & el.classList.contains(`yachtOn`)) {
+      el.classList.add(`clearYachtOn`);
+      yachtY();
+      setTimeout(() => {
+        el.classList.remove(`yachtOn`, `clearYachtOn`);
+      }, 510);
+    }
+  });
 };
-const clearBtns = () => {};
+const yachtX = () => {
+  bigBtns.forEach((el) => {
+    if (el.classList.contains(`yacht`)) {
+      el.classList.add(`yachtOn`);
+    }
+  });
+};
+const yachtY = () => {
+  bigBtns.forEach((el) => {
+    if (el.classList.contains(`destination`)) {
+      el.classList.add(`yachtOn`);
+    }
+  });
+};
 
 yachts.addEventListener(`click`, handleClickYachts);
 destination.addEventListener(`click`, handleClickDestination);
