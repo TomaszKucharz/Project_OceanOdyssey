955fe04550c419d1c4b8b13c7eeed720491fb189
Tomasz Kucharz <tkucharz@frontendwebsite.com>
2022-07-10 19:39:07 +0200

    9.07.2022

--- js/menu.js
+++ js/menu.js
@@ -4,19 +4,33 @@ const menuLi = document.querySelector(`.menuLi`);
 const contact = document.querySelector(`.contactForUs`);
 const bigBtns = [...document.querySelectorAll(`.bigBtn`)];
 
-const handleClikYachts = () => {
+const handleClickYachts = () => {
 	bigBtns.forEach((el) => {
 		if (el.classList.contains(`yacht`)) {
 			el.classList.toggle(`yachtOn`);
-		} else {
+		} else if (el.classList.contains(`destination`) & el.classList.contains(`yachtOn`)) {
+			el.classList.add(`clearYachtOn`);
 		}
 	});
 };
-const handleClikDestination = () => {};
-
+const handleClickDestination = () => {
+	bigBtns.forEach((el) => {
+		if (el.classList.contains(`destination`)) {
+			el.classList.toggle(`yachtOn`);
+		} else if (el.classList.contains(`yacht`) & el.classList.contains(`yachtOn`)) {
+			el.classList.add(`clearYachtOn`);
+			// bigBtns.forEach((el) => {
+			// 	if (el.classList.contains(`clearYachtOn`)) {
+			// 		el.classList.remove(`clearYachtOn`);
+			// 		el.classList.remove(`yachtOn`);
+			// 	}
+			// });
+		}
+	});
+};
 const clearBtns = () => {};
 
-yachts.addEventListener(`click`, handleClikYachts);
-destination.addEventListener(`click`, handleClikDestination);
+yachts.addEventListener(`click`, handleClickYachts);
+destination.addEventListener(`click`, handleClickDestination);
 // menuLi.addEventListener(`click`, showMenu);
 // contact.addEventListener(`click`, showContact);
