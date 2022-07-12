dead1bb12e5445b27b4b217bd3fe7507a65afa8d
Tomasz Kucharz <tkucharz@frontendwebsite.com>
2022-07-12 23:21:19 +0200

    dark/light mode finish

--- js/switch.js
+++ js/switch.js
@@ -1,14 +1,17 @@
 const sun = document.querySelector(`.fa-sun`);
 const moon = document.querySelector(`.fa-moon`);
+const body = document.querySelector(`body`);
 
 const showDark = () => {
-  sun.classList.toggle(`light`);
-  moon.classList.toggle(`light`);
+  body.setAttribute(`data-mode`, "dark");
+  moon.style.display = "none";
+  sun.style.display = "block";
 };
-const showLight = () => {
-  sun.classList.toggle(`light`);
-  moon.classList.toggle(`light`);
+const showLight = (params) => {
+  body.setAttribute(`data-mode`, "light");
+  moon.style.display = "block";
+  sun.style.display = "none";
 };
 
-sun.addEventListener(`click`, showDark);
-moon.addEventListener(`click`, showLight);
+sun.addEventListener(`click`, showLight);
+moon.addEventListener(`click`, showDark);
