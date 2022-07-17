ee652bc31606ad3b3eafcf2a5a7198a7f8fcbe4d
Tomasz Kucharz <tkucharz@frontendwebsite.com>
2022-07-17 11:46:44 +0200

    form ThinkPad E15

--- js/menu.js
+++ js/menu.js
@@ -11,102 +11,87 @@ const offsetForm2 = document.querySelector(`.form2`).offsetTop;
 
 //scrole
 window.addEventListener(`scroll`, function () {
-  if (
-    document.body.scrollTop > 100 ||
-    document.documentElement.scrollTop > 100
-  ) {
-    banner.className = `banner off`;
-  } else {
-    banner.className = `banner on`;
-  }
+	if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+		banner.className = `banner off`;
+	} else {
+		banner.className = `banner on`;
+	}
 });
 window.addEventListener(`scroll`, function () {
-  if (
-    document.body.scrollTop > offsetForm2 ||
-    document.documentElement.scrollTop > offsetForm2
-  ) {
-    pageBack.style.visibility = "visible";
-  } else {
-    pageBack.style.visibility = "hidden";
-  }
+	if (document.body.scrollTop > offsetForm2 || document.documentElement.scrollTop > offsetForm2) {
+		pageBack.style.visibility = 'visible';
+	} else {
+		pageBack.style.visibility = 'hidden';
+	}
 });
 window.addEventListener(`scroll`, function () {
-  if (
-    document.body.scrollTop > 200 ||
-    document.documentElement.scrollTop > 200
-  ) {
-    menuB.classList.add(`mbOn`);
-  } else {
-    menuB.classList.remove(`mbOn`);
-  }
+	if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+		menuB.classList.add(`mbOn`);
+	} else {
+		menuB.classList.remove(`mbOn`);
+	}
 });
 window.addEventListener(`scroll`, function () {
-  if (
-    document.body.scrollTop > offsetForm2 ||
-    document.documentElement.scrollTop > offsetForm2
-  ) {
-    clearBbtns();
-  }
+	if (document.body.scrollTop > offsetForm2 || document.documentElement.scrollTop > offsetForm2) {
+		clearBbtns();
+	}
 });
 const handleClickYachts = () => {
-  clearList();
-  bigBtns.forEach((el) => {
-    const xxx = el.classList.contains;
-    if (
-      el.classList.contains(`destination`) & el.classList.contains(`yachtOn`)
-    ) {
-      el.classList.add(`clearYachtOn`);
-      setTimeout(() => {
-        el.classList.remove(`yachtOn`, `clearYachtOn`);
-      }, 510);
-    } else if (el.classList.contains(`yacht`)) {
-      el.classList.toggle(`yachtOn`);
-    }
-  });
+	clearList();
+	bigBtns.forEach((el) => {
+		const xxx = el.classList.contains;
+		if (el.classList.contains(`destination`) & el.classList.contains(`yachtOn`)) {
+			el.classList.add(`clearYachtOn`);
+			setTimeout(() => {
+				el.classList.remove(`yachtOn`, `clearYachtOn`);
+			}, 510);
+		} else if (el.classList.contains(`yacht`)) {
+			el.classList.toggle(`yachtOn`);
+		}
+	});
 };
 
 const handleClickDestination = () => {
-  clearList();
-  bigBtns.forEach((el) => {
-    if (el.classList.contains(`destination`)) {
-      el.classList.toggle(`yachtOn`);
-    } else if (
-      el.classList.contains(`yacht`) & el.classList.contains(`yachtOn`)
-    ) {
-      el.classList.add(`clearYachtOn`);
-      setTimeout(() => {
-        el.classList.remove(`yachtOn`, `clearYachtOn`);
-      }, 510);
-    }
-  });
+	clearList();
+	bigBtns.forEach((el) => {
+		if (el.classList.contains(`destination`)) {
+			el.classList.toggle(`yachtOn`);
+		} else if (el.classList.contains(`yacht`) & el.classList.contains(`yachtOn`)) {
+			el.classList.add(`clearYachtOn`);
+			setTimeout(() => {
+				el.classList.remove(`yachtOn`, `clearYachtOn`);
+			}, 510);
+		}
+	});
 };
 const clearList = () => {
-  menuB.classList.remove(`moveBar`);
+	menuB.classList.remove(`moveBar`);
 };
 const showContact = () => {
-  console.log("text");
-  //tu wstawić kod włączający widoczność buttona back
-  pageBack.style.visibility = "visible";
-  const form4 = document.querySelector(`.form4`).offsetTop;
-  window.scrollTo({ top: form4, behavior: "smooth" });
+	console.log('text');
+	clearMenu();
+	//tu wstawić kod włączający widoczność buttona back
+	pageBack.style.visibility = 'visible';
+	const form4 = document.querySelector(`.form4`).offsetTop;
+	window.scrollTo({ top: form4, behavior: 'smooth' });
 };
 const pushBack = () => {
-  clearBbtns();
-  clearMenu();
-  window.scrollTo({ top: 0, behavior: "smooth" });
+	clearBbtns();
+	clearMenu();
+	window.scrollTo({ top: 0, behavior: 'smooth' });
 };
 
 const showMenu = () => {
-  clearBbtns();
-  menuB.classList.toggle(`moveBar`);
+	clearBbtns();
+	menuB.classList.toggle(`moveBar`);
 };
 const clearBbtns = () => {
-  bigBtns.forEach((el) => {
-    el.classList.remove("yachtOn");
-  });
+	bigBtns.forEach((el) => {
+		el.classList.remove('yachtOn');
+	});
 };
 const clearMenu = () => {
-  menuB.classList.remove(`moveBar`);
+	menuB.classList.remove(`moveBar`);
 };
 yachts.addEventListener(`click`, handleClickYachts);
 destination.addEventListener(`click`, handleClickDestination);
